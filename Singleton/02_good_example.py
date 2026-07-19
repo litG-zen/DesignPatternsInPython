5679ccd7f096b8b1ef4c2a62b3842b35226894db
Lit_Coder <lalit.coder2296@gmail.com>
2026-07-19 07:31:45 +0530

    logger update

--- Singleton/02_good_example.py
+++ Singleton/02_good_example.py
@@ -1,20 +1,27 @@
 class Logger:
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls,file_name):
         if cls._instance is None:
             cls._instance = super(Logger, cls).__new__(cls)
-            # Initialize any attributes here
+            cls._instance.file_name = file_name
+            cls._instance.log_count = 0
         return cls._instance
 
     def log(self, message):
         print(f"Log: {message}")
+        self.log_count += 1
 
 
 
-log1 = Logger()
-log2 = Logger()
+log1 = Logger("main")
+log2 = Logger("main")
 
 print(log1 is log2)  # Output: True
 print(id(log1))
 print(id(log2))
+
+print(log1.log("User created"))
+print(log2.log("OTP sent"))
+print(log1.log_count)  # Output: 2
+print(log2.log_count)  # Output: 2
